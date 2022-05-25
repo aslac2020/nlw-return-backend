6d0350de4ed54b1cafefbe45f14ef2bcb6e3a57a
André de Sousa Lacerda <a.de.sousa.lacerda@avanade.com>
2022-05-25 16:12:32 -0300

    Adjusts

--- src/routes.ts
+++ src/routes.ts
@@ -8,16 +8,23 @@ export const routes = express.Router();
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
+  try{
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
+  
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+  
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    });
+  
+    return res.status(201).send();
+  }catch(err){
+    console.log(err);
 
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+    return res.status(500).send();
+  }
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  });
-
-  return res.status(201).send();
 });
